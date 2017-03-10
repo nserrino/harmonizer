a32330fdefdb9507ddd62e1e174f38dc4adb9174
Natalie <natalie.serrino@gmail.com>
2017-03-10 12:52:47 -0800

    refactor works on single file

--- rock-corpus/parse_rock.py
+++ rock-corpus/parse_rock.py
@@ -2,9 +2,10 @@ import argparse
 import json
 import os
 
-INPUT_HARMONY = '/Users/natalieserrino/harmony/rock-corpus/rs200_harmony_clt/1999_tdc.clt'
-INPUT_MELODY = '/Users/natalieserrino/harmony/rock-corpus/rs200_melody_nlt/1999_tdc.nlt'
-OUPTUT_FILE = '/Users/natalieserrino/harmony/rock-corpus/1999_tdc.csv'
+CLEAN = True
+MELODY_EXT = ".nlt"
+HARMONY_EXT = ".clt"
+
 
 def get_harmony_data(filepath):
     harmony_file = open(filepath, 'r')
@@ -110,17 +111,47 @@ def merge_harmony_and_melody(harmony, melody):
     return output_data
 
 
+def parse_and_write_song(harmony_path, melody_path, output_path):
+    output_dir = os.path.dirname(output_path)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    if os.path.exists(output_path):
+        if CLEAN:
+            os.remove(output_path)
+        else:
+            raise Exception("Output path exists: " + output_path)
+
+    harmony = get_harmony_data(harmony_path)
+    melody = get_melody_data(melody_path)
+    result = merge_harmony_and_melody(harmony, melody)
+    f = open(output_path, 'w')
+    f.write(json.dumps(result))
+    f.close()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--harmony", help="Path to harmony file(s)", required=True)
     parser.add_argument("--melody", help="Path to melody file(s)", required=True)
     parser.add_argument("--dest", help="Output destination", required=True)
     args = parser.parse_args()
+    harmony_path = os.path.abspath(args.harmony)
+    melody_path = os.path.abspath(args.melody)
+    dest_path = os.path.abspath(args.dest)
 
-    harmony = get_harmony_data(os.path.abspath(args.harmony))
-    melody = get_melody_data(os.path.abspath(args.melody))
+    if os.path.isdir(harmony_path) and os.path.isdir(melody_path):
+        # cut off extension
+        harmony_songs = [f[:-4] for f in os.listdir(harmony_path) if f.endswith(HARMONY_EXT)]
+        melody_songs = [f[:-4] for f in os.listdir(melody_path) if f.endswith(MELODY_EXT)]
+        songs = set.intersection(set(harmony_songs), set(melody_songs))
 
-    f = open(os.path.abspath(args.dest), 'w')
-    result = merge_harmony_and_melody(harmony, melody)
-    f.write(json.dumps(result))
-    f.close()
+        for song in songs:
+            harmony = os.path.join(harmony_path, song + HARMONY_EXT)
+            melody = os.path.join(melody_path, song + MELODY_EXT)
+            output = os.path.join(dest_path, song + ".json")
+            parse_and_write_song(harmony, melody, output)
+
+        print "Wrote", len(songs), "songs to", dest_path
+    else:
+        parse_and_write_song(harmony_path, melody_path, dest_path)
